0a5eba87f206a5180b4cea021a1eff9265acbb2a
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 02:05:59 +0400

    Update Tests.ps1

--- test/Tests.ps1
+++ test/Tests.ps1
@@ -4,6 +4,20 @@ Describe 'Get-Secret' {
 }
 
 Describe 'Set-Secret' {
+    BeforeAll {
+        . .\test\Helpers.ps1
+        CommonBeforeAll
+    }
+    AfterAll {
+        . .\test\Helpers.ps1
+        CommonAfterAll
+    }
+
+    It 'For string' {
+        $name = "Name-$(Get-Random)"
+        $secret = "Secret-$(Get-Random)"
+        Add-Secret -Name $name -Secret $secret -Vault $Script:VaultName
+    }
 }
 
 Describe 'Remove-Secret' {

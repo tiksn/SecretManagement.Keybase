71045ce2c4d24a1af88ff0283bad341711e9e9f7
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 03:42:06 +0400

    Update psakefile.ps1

--- psakefile.ps1
+++ psakefile.ps1
@@ -10,19 +10,6 @@ Task PublishPSGallery -depends PublishLocally {
     Publish-Module -Name KeybaseSecretManagementExtension -Repository PSGallery -NuGetApiKey $apiKey
 }
 
-Task PublishLocally -depends Test {
-    $documentsDir = [Environment]::GetFolderPath("MyDocuments")
-    $powerShellDir = Join-Path -Path $documentsDir -ChildPath 'PowerShell'
-    $modulesDir = Join-Path -Path $powerShellDir -ChildPath 'Modules'
-    $moduleDir = Join-Path -Path $modulesDir -ChildPath 'KeybaseSecretManagementExtension'
-
-    if (! (Test-Path -Path $moduleDir)) {
-        New-Item -Path $moduleDir -ItemType Directory | Out-Null
-    }
-
-    Copy-Item -Path .\src\* -Destination $moduleDir -Recurse -Force
-}
-
 Task Test -Depends ImportModule {
     $result = Invoke-Pester -Script .\test\Tests.ps1 -PassThru
     if ($result.FailedCount -ne 0) {
@@ -30,14 +17,29 @@ Task Test -Depends ImportModule {
     }
 }
  
-Task ImportModule -Depends RemoveModule {
+Task ImportModule -Depends PublishLocally {
     Import-Module .\src\KeybaseSecretManagementExtension.psd1
     
     if (!(Get-Module -Name KeybaseSecretManagementExtension)) {
         throw 'Failed to import module.'
     }
 }
- 
+
+Task PublishLocally -depends RemoveModule {
+    $documentsDir = [Environment]::GetFolderPath("MyDocuments")
+    $powerShellDir = Join-Path -Path $documentsDir -ChildPath 'PowerShell'
+    $modulesDir = Join-Path -Path $powerShellDir -ChildPath 'Modules'
+    $moduleDir = Join-Path -Path $modulesDir -ChildPath 'KeybaseSecretManagementExtension'
+
+    if (Test-Path -Path $moduleDir) {
+        Remove-Item $moduleDir -Force -Recurse
+    }
+
+    New-Item -Path $moduleDir -ItemType Directory | Out-Null
+
+    Copy-Item -Path .\src\* -Destination $moduleDir -Recurse -Force
+}
+
 Task RemoveModule {
     if (Get-Module -Name KeybaseSecretManagementExtension) {
         Remove-Module -Name KeybaseSecretManagementExtension

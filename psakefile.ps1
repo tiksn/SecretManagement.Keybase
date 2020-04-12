52e26c42cf9ce30b0d005b2ddc938cdad0ecf842
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 05:15:58 +0400

    Update psakefile.ps1

--- psakefile.ps1
+++ psakefile.ps1
@@ -12,6 +12,27 @@ Task PublishPSGallery -depends PublishLocally {
 
 Task Test -Depends ImportModule {
     $result = Invoke-Pester -Script .\test\Tests.ps1 -PassThru
+
+    $leftover = $null
+    '{"method": "list", "params": {"options": {"namespace": "test"}}}' | keybase kvstore api | Set-Variable leftover
+    $leftover = $leftover | ConvertFrom-Json -AsHashtable -Depth 4
+    $namespace = $leftover['result']['namespace']
+    $entryKeys = $leftover['result']['entryKeys']
+
+    foreach ($entryKey in $entryKeys) {
+        $key = $entryKey['entryKey']
+
+        @{
+            'method' = 'del'
+            'params' = @{ 
+                'options' = @{
+                    'namespace' = $namespace
+                    'entryKey'  = $key
+                }
+            }
+        } | ConvertTo-Json -Depth 4 | keybase kvstore api
+    }
+
     if ($result.FailedCount -ne 0) {
         throw 'Tests failed'
     }

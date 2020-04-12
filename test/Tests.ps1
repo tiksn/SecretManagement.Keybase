366e24ae7788a3bc8f25d62d2464e74f05798519
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 12:22:26 +0400

    Update Tests.ps1

--- test/Tests.ps1
+++ test/Tests.ps1
@@ -1,6 +1,24 @@
 Set-StrictMode -Version Latest
 
 Describe 'Get-Secret' {
+    It 'For ByteArray' {
+        
+    }
+
+    It 'For string' {
+    }
+
+    It 'For SecureString' {
+
+    }
+
+    It 'For PSCredential' {
+
+    }
+
+    It 'For Hashtable' {
+
+    }
 }
 
 Describe 'Set-Secret' {
@@ -13,17 +31,69 @@ Describe 'Set-Secret' {
         CommonAfterAll
     }
 
+    It 'For ByteArray' {
+        
+    }
+
     It 'For string' {
         $name = "Name-$(Get-Random)"
         $secret = "Secret-$(Get-Random)"
         Add-Secret -Name $name -Secret $secret -Vault $Script:VaultName
     }
+
+    It 'For SecureString' {
+        
+    }
+
+    It 'For PSCredential' {
+        
+    }
+
+    It 'For Hashtable' {
+        
+    }
 }
 
 Describe 'Remove-Secret' {
+    It 'For ByteArray' {
+        
+    }
+
+    It 'For string' {
+    }
+
+    It 'For SecureString' {
+        
+    }
+
+    It 'For PSCredential' {
+        
+    }
+
+    It 'For Hashtable' {
+        
+    }
 }
 
 Describe 'Get-SecretInfo' {
+    It 'For ByteArray' {
+        
+    }
+
+    It 'For string' {
+    }
+
+    It 'For SecureString' {
+        
+    }
+
+    It 'For PSCredential' {
+        
+    }
+
+    It 'For Hashtable' {
+        
+    }
 }
 
 Describe 'Test-SecretVault' {

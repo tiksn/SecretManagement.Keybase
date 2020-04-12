9af4d1fb3cb6415c7c96de13b978cb035f3b509a
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 14:23:53 +0400

    Update Get-Secret.Tests.ps1

--- test/Get-Secret.Tests.ps1
+++ test/Get-Secret.Tests.ps1
@@ -6,6 +6,12 @@ Describe 'Get-Secret' {
     }
 
     It 'For string' {
+        $name = "Name-$(Get-Random)"
+        $secret = "Secret-$(Get-Random)"
+        Add-Secret -Name $name -Secret $secret -Vault $Script:VaultName
+        $retrievedSecret = Get-Secret -Name $name -Vault $Script:VaultName -AsPlainText
+
+        $secret | Should -Be $retrievedSecret
     }
 
     It 'For SecureString' {

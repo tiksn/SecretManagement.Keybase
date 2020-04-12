3884dc165aaf1fd05d29469307886c1706239ab9
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 04:53:20 +0400

    Update SecretsManagementExtension.psm1

--- src/SecretsManagementExtension/SecretsManagementExtension.psm1
+++ src/SecretsManagementExtension/SecretsManagementExtension.psm1
@@ -1,3 +1,62 @@
+function ConvertTo-MultiformatString {
+    param (
+        [Parameter(Mandatory = $false)]
+        [object]
+        $EntryValue
+    )
+    $EntryValue.ToString() #TODO: Replace with real implementation
+}
+
+function Invoke-ApiCall {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Method,
+        [Parameter(Mandatory = $false)]
+        [hashtable]
+        $AdditionalParameters,
+        [Parameter(Mandatory = $false)]
+        [string]
+        $EntryKey,
+        [Parameter(Mandatory = $false)]
+        [Nullable[int]]
+        $Revision,
+        [Parameter(Mandatory = $false)]
+        [object]
+        $EntryValue
+    )
+    
+    $requestOptions = @{ }
+
+    if ($AdditionalParameters.ContainsKey('team')) {
+        $requestOptions.Add('team', $AdditionalParameters['team']);
+    }
+
+    if ($AdditionalParameters.ContainsKey('namespace')) {
+        $requestOptions.Add('namespace', $AdditionalParameters['namespace']);
+    }
+
+    if ($null -ne $EntryKey) {
+        $requestOptions.Add('entryKey', $EntryKey);
+    }
+
+    if ($null -ne $Revision) {
+        $requestOptions.Add('revision', $Revision);
+    }
+
+    if ($null -ne $EntryValue) {
+        $formattedValue = ConvertTo-MultiformatString -EntryValue $EntryValue
+        $requestOptions.Add('entryValue', $formattedValue);
+    }
+
+    $request = @{ 'method' = $Method; 'params' = @{'options' = $requestOptions} }
+    $json = $request | ConvertTo-Json
+    $resultJson = $json | keybase kvstore api
+    $result = $resultJson | ConvertFrom-Json -Depth 2 -AsHashtable
+
+    return $result
+}
+
 function Get-Secret {
     param (
         [string] $Name,
@@ -13,6 +72,16 @@ function Set-Secret {
         [string] $VaultName,
         [hashtable] $AdditionalParameters
     )
+
+    $result = Invoke-ApiCall -Method 'put' -AdditionalParameters $AdditionalParameters -EntryKey $Name -EntryValue $Secret
+
+    if ($null -eq $result.error.message) {
+        return $true
+    }
+    else {
+        Write-Error $result.error.message
+        return $false
+    }
 }
 
 function Remove-Secret {

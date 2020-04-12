7dcd83ae5477d8f8e15e2a19609a985978aff5dd
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-12 15:40:53 +0400

    Update Test-SecretVault.Tests.ps1

--- test/Test-SecretVault.Tests.ps1
+++ test/Test-SecretVault.Tests.ps1
@@ -1,4 +1,12 @@
 Set-StrictMode -Version Latest
 
 Describe 'Test-SecretVault' {
+    BeforeAll {
+        . .\test\Helpers.ps1
+        CommonBeforeAll
+    }
+    AfterAll {
+        . .\test\Helpers.ps1
+        CommonAfterAll
+    }
 }
